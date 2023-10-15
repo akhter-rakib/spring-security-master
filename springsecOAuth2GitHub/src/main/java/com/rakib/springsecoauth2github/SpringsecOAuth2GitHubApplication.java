748541b11f4191ebb1ecd25b0563e3d42fe717d8
AktaruzzamanRakib <aktaruzzaman.rakib@bjitgroup.com>
2023-10-15 20:38:01 +0600

    project initiate with base dependency for OAuth2
    -- added OAuth2Client, Spring Security and Web Dependency

---
+++ springsecOAuth2GitHub/src/main/java/com/rakib/springsecoauth2github/SpringsecOAuth2GitHubApplication.java
@@ -0,0 +1,13 @@
+package com.rakib.springsecoauth2github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecOAuth2GitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecOAuth2GitHubApplication.class, args);
+	}
+
+}
---
+++ springsecOAuth2GitHub/src/test/java/com/rakib/springsecoauth2github/SpringsecOAuth2GitHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakib.springsecoauth2github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecOAuth2GitHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
