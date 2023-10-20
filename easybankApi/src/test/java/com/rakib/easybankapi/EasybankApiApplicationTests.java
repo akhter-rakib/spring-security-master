92655df5e6671b2bc808b5374db290278d6a30c9
AktaruzzamanRakib <aktaruzzaman.rakib@bjitgroup.com>
2023-10-20 11:56:39 +0600

    easy bank project initiate

---
+++ easybankApi/src/test/java/com/rakib/easybankapi/EasybankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakib.easybankapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasybankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
