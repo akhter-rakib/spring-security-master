41c9627fb68568db4636bc8059be45e92de92d6d
Rakib <rakibccj@gmail.com>
2024-07-06 01:05:16 +0600

    project initiate

---
+++ spring-okta-oauth2/src/main/java/com/rakib/springoktaoauth2/SpringOktaOauth2Application.java
@@ -0,0 +1,13 @@
+package com.rakib.springoktaoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOktaOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOktaOauth2Application.class, args);
+    }
+
+}
---
+++ spring-okta-oauth2/src/test/java/com/rakib/springoktaoauth2/SpringOktaOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakib.springoktaoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOktaOauth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
