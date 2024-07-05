91b5dfd89cb07689bc39da8a301a2fac22c02a27
Rakib <rakibccj@gmail.com>
2024-07-06 02:55:07 +0600

    OktaOAuth2WebSecurity configuration

---
+++ spring-okta-oauth2/src/main/java/com/rakib/springoktaoauth2/controller/AuthenticateController.java
@@ -0,0 +1,50 @@
+package com.rakib.springoktaoauth2.controller;
+
+import com.rakib.springoktaoauth2.model.AuthenticationResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
+
+@RestController
+@RequestMapping("/api")
+public class AuthenticateController {
+
+    @GetMapping("/public/hello")
+    public String publicHello() {
+        return "Hello, this is a public endpoint!";
+    }
+
+    // Secure endpoint
+    @GetMapping("/secure/hello")
+    public ResponseEntity<AuthenticationResponse> secureHello(
+            @AuthenticationPrincipal OidcUser oidcUser,
+            Model model,
+            @RegisteredOAuth2AuthorizedClient("okta")
+            OAuth2AuthorizedClient client
+
+    ) {
+        AuthenticationResponse authenticationResponse
+                = AuthenticationResponse.builder()
+                .userId(oidcUser.getEmail())
+                .accessToken(client.getAccessToken().getTokenValue())
+                .refreshToken(Objects.requireNonNull(client.getRefreshToken()).getTokenValue())
+                .expiresAt(Objects.requireNonNull(client.getAccessToken().getExpiresAt()).getEpochSecond())
+                .authorityList(oidcUser.getAuthorities()
+                        .stream()
+                        .map(GrantedAuthority::getAuthority)
+                        .toList())
+                .build();
+        return new ResponseEntity<>(authenticationResponse, HttpStatus.OK);
+    }
+
+}
---
+++ spring-okta-oauth2/src/main/java/com/rakib/springoktaoauth2/security/OktaOAuth2WebSecurity.java
@@ -0,0 +1,55 @@
+package com.rakib.springoktaoauth2.security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class OktaOAuth2WebSecurity {
+
+    @Value("${okta.oauth2.issuer}")
+    private String issuer;
+    @Value("${okta.oauth2.post-logout-redirect-uri}")
+    private String postLogoutRedirectUri;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/public/**").permitAll() // Specify public endpoints
+                                .anyRequest().authenticated()
+                )
+                .oauth2Login(withDefaults())
+                .oauth2ResourceServer(oauth2ResourceServer ->
+                        oauth2ResourceServer.jwt(withDefaults())
+                ).logout(logout -> logout
+                        .logoutSuccessHandler(oidcLogoutSuccessHandler())
+                );
+        return http.build();
+    }
+
+    private LogoutSuccessHandler oidcLogoutSuccessHandler() {
+        return (HttpServletRequest request, HttpServletResponse response, org.springframework.security.core.Authentication authentication) -> {
+            if (authentication != null && authentication.getPrincipal() instanceof OidcUser) {
+                OidcUser oidcUser = (OidcUser) authentication.getPrincipal();
+                String idToken = oidcUser.getIdToken().getTokenValue();
+                String logoutUrl = issuer + "/v1/logout?id_token_hint=" + idToken
+                        + "&post_logout_redirect_uri=" + postLogoutRedirectUri;
+                response.sendRedirect(logoutUrl);
+            } else {
+                response.sendRedirect("/");
+            }
+        };
+    }
+}
