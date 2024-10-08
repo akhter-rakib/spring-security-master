3d1fca77200abc1b4d1d5d9322456760f2d53681
aktaruzzaman.rakib <aktaruzzaman.rakib@bjitgroup.com>
2024-10-09 00:32:22 +0600

    azure active directory

---
+++ azure-auth/src/main/java/com/rakib/azureauth/AzureAuthApplication.java
@@ -0,0 +1,13 @@
+package com.rakib.azureauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzureAuthApplication.class, args);
+    }
+
+}
